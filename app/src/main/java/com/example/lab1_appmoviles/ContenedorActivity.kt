05786db50735d77d5c356c411e43e2bbeb9b2294
MelinaCora <melinaccora97@gmail.com>
2026-05-07 19:05:30 -0300

    Configuracion inicial de ContenedorActivity y Fragments

--- app/src/main/java/com/example/lab1_appmoviles/ContenedorActivity.kt
+++ app/src/main/java/com/example/lab1_appmoviles/ContenedorActivity.kt
@@ -9,12 +9,13 @@ import androidx.core.view.WindowInsetsCompat
 class ContenedorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_contenedor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Solo cargamos el Fragment inicial si es la primera vez.
+        // Si savedInstanceState != null, el sistema ya restauro el Fragment.
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, PrincipalFragment())
+                .commit()
         }
     }
 }

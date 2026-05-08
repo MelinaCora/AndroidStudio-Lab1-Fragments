4088861262ac47ace233a97985cca7ea9cba28cd
MelinaCora <melinaccora97@gmail.com>
2026-05-08 18:06:33 -0300

    boton volver y saludo personalizado

--- app/src/main/java/com/example/lab1_appmoviles/PrincipalFragment.kt
+++ app/src/main/java/com/example/lab1_appmoviles/PrincipalFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
@@ -35,6 +37,52 @@ class PrincipalFragment : Fragment() {
         (activity as AppCompatActivity)
             .supportActionBar
             ?.title = "Bienvenida"
-    }
 
+
+        // Referencias XML
+        val editText =
+            view.findViewById<EditText>(R.id.editTextNombre)
+
+        val boton =
+            view.findViewById<AppCompatButton>(R.id.buttonContinuar)
+
+        // Evento botón
+        boton.setOnClickListener {
+
+            // Leer texto
+            val nombre = editText.text.toString()
+
+            // Validación
+            if (nombre.isBlank()) {
+
+                editText.error = "Ingresa tu nombre"
+
+                return@setOnClickListener
+            }
+
+            // Crear DetalleFragment
+            val detalleFragment = DetalleFragment()
+
+            // Crear Bundle
+            val bundle = Bundle()
+
+            bundle.putString(
+                "NOMBRE_USUARIO",
+                nombre
+            )
+
+            // Mandar datos al Fragment
+            detalleFragment.arguments = bundle
+
+            // Navegar al Fragment
+            parentFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragmentContainer,
+                    detalleFragment
+                )
+                .addToBackStack(null)
+                .commit()
+        }
+    }
 }
